9dfc5916e8220e11290470f2c03f24e60ed3f35a
1q2w3e4r <popinho7@gmail.com>
2019-08-29 14:34:43 +0200

    update
    test

---
+++ config.py
@@ -0,0 +1,4 @@
+# .gitignore should include reference to config.py
+x_league = "678206"
+x_user = "3929462"
+authorization = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOjI2MzMwMzczLCJpYXQiOjE1NjQ5MzE0ODJ9.SqwGi_WgVxlDEjIH8EaF_NkvXmdwRooZT93xbeRFLWM"
